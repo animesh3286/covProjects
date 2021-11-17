0f9357be64634c0ecf3872ac12e2f3bc222f9f50
animesh.kumar <animesh.kumar@AnimeshKumar.covalensedigital.net>
2021-11-17 11:36:05 +0530

    Initial Commit

---
+++ SpringDemo_02/src/com/cov/app/App_02.java
@@ -0,0 +1,21 @@
+package com.cov.app;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.cov.beans.Student;
+import com.cov.beans.Triangle;
+
+public class App_02 {
+	public static void main(String args[]) {
+		ApplicationContext ctxt = new ClassPathXmlApplicationContext("spring.xml");
+		Student student1 = (Student)ctxt.getBean("stud");
+		System.out.println("Id: "+student1.getId()+"Name :"+student1.getName());
+		
+		Triangle triangle = (Triangle)ctxt.getBean("tri");
+		System.out.println(triangle.getPointA()+" "+triangle.getPointB()+" "+triangle.getPointC());
+	}
+}
+			
+
+
